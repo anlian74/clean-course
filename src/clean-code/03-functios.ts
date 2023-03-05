1a1f06ca92750d3909957f85357a68f74e8869d7
Ángel Suárez <jasr74@gmail.com>
2023-03-05 19:17:17 +0000

    Ejercicio 3

---
+++ src/clean-code/03-functios.ts
@@ -0,0 +1,42 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieById( id: string ) {
+        console.log({ movieId: id });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById( id: string ) {
+        console.log({ id });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorById( Id: string ) {
+        console.log({ ActorId: Id });
+    }
+    
+    // Crear una película
+    interface Movie {
+        cast: string[];
+        description: string; 
+        rating: number; 
+        title: string; 
+    }
+
+    function createMovie({title, description, rating, cast}: Movie ){
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if ( fullName === 'fernando' ) return false;
+
+        console.log('Crear actor', birthdate);
+        return true;        
+
+    }
+})();
