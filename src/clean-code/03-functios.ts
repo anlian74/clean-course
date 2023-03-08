b8e059c5d4e26ce3ca30a2e32980e1abce69f171
Ángel Suárez <jasr74@gmail.com>
2023-03-08 19:54:01 +0000

    Ejercicio 04

--- src/clean-code/03-functios.ts
+++ src/clean-code/03-functios.ts
@@ -37,6 +37,16 @@
 
         console.log('Crear actor', birthdate);
         return true;        
+    }
 
+    // Continuación
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }): number => {
+        
+        if ( isDead ) return 1500;
+        
+        if ( isSeparated ) return 2500;
+
+        return ( isRetired ) ? 3000 : 4000;
     }
 })();
